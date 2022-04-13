3cc8778ebf65254a4c562f6404edfb8231d41a8e
Ilya-ilik <101934601+Ilya-ilik@users.noreply.github.com>
2022-04-13 23:45:49 +0300

    Currency converter on JS v.1
    Currency converter on JS v.1

---
+++ script.js
@@ -0,0 +1,9 @@
+function moneyConverter(valNum) {
+    document.getElementById("outputrub").innerHTML = valNum*60
+}
+
+//moneyConverter(valNum) в скобках после имени функции указано что будем здесь использовать значение числа
+//document.getElementById("outputrub").innerHTML = valNum*60
+//document.getElementById("outputrub") получение элемента по id outputrub,
+//.innerHTML это св-ство html
+//.innerHTML = valNum*60 наше значение умножаем на 60 рублей 
